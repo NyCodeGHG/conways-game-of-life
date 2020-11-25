9855efe709307d32429e2e5cdb180e7eb5dfa249
NyCodeGHG <nycode@protonmail.com>
2020-11-25 16:09:07 +0100

    Removed old stuff

--- src/main/kotlin/de/nycode/gameoflife/Main.kt
+++ src/main/kotlin/de/nycode/gameoflife/Main.kt
@@ -3,27 +3,9 @@ package de.nycode.gameoflife
 import de.nycode.gameoflife.game.GameLoop
 import kotlinx.browser.window
 
-private var fps = 0
-private var lastFrame: Double? = null
-
 fun main() {
-    val gameLoop = GameLoop(100, 100, 8.0)
+    val gameLoop = GameLoop(150, 150, 5.0)
     window.setTimeout({
         gameLoop.start()
     }, 1000)
 }
-
-private val times = arrayListOf<Double>()
-
-//fun render(ctx: CanvasRenderingContext2D) {
-//    // Calculate fps
-//    val now = window.performance.now()
-//    while (times.isNotEmpty() && times[0] <= now - 1000) {
-//        times.removeFirst()
-//    }
-//    times.add(now)
-//    fps = times.size
-//
-//    ctx.fillStyle = "white"
-//    ctx.fillText("$fps FPS", 20.0, 20.0)
-//}
