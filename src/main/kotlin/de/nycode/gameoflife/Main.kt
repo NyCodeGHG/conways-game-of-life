540dcc3e6a36100db244016d2aeb4de0efe93bca
NyCodeGHG <nycode@protonmail.com>
2020-11-26 15:16:53 +0100

    Updated speed and better description in KDocs

--- src/main/kotlin/de/nycode/gameoflife/Main.kt
+++ src/main/kotlin/de/nycode/gameoflife/Main.kt
@@ -16,14 +16,15 @@ fun main() {
 object Options {
 
     /**
-     * The speed how long the [GameLoop] waits before rendering the next frame in milliseconds
+     * The time how long the [GameLoop] waits before rendering the next frame in milliseconds
+     * lower = faster
      */
-    const val updateSpeed = 100
+    const val updateSpeed = 50
 
     /**
      * The chance of a cell to be alive in percent
      */
-    const val aliveChancePercentage = 10.0
+    const val aliveChancePercentage = 31.25
 
     /**
      * If the neighbours behave like in a torus ("like in pac man") or the walls are just [de.nycode.gameoflife.cell.BorderCell]s
